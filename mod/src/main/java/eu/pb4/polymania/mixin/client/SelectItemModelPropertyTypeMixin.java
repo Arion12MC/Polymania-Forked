98f294282f051e94f36ca40c415ef13e3fa35cd4
Patbox <39821509+Patbox@users.noreply.github.com>
2026-01-09 20:51:41 +0100

    Update to 1.21.11 (0.6.0-beta.1)

--- mod/src/main/java/eu/pb4/polymania/mixin/client/SelectItemModelPropertyTypeMixin.java
+++ mod/src/main/java/eu/pb4/polymania/mixin/client/SelectItemModelPropertyTypeMixin.java
@@ -2,14 +2,14 @@ package eu.pb4.polymania.mixin.client;
 
 import com.google.common.collect.Multiset;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.client.render.item.property.select.SelectProperty;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.Set;
+import net.minecraft.client.renderer.item.properties.select.SelectItemModelProperty;
 
-@Mixin(SelectProperty.Type.class)
+@Mixin(SelectItemModelProperty.Type.class)
 public class SelectPropertyTypeMixin {
     @Redirect(method = "validateCases", at = @At(value = "INVOKE", target = "Ljava/util/Set;size()I"))
     private static int replaceSize(Set instance, @Local Multiset multiset) {
